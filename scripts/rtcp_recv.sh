d44b55f464018150e981f6c3309665c1c2e302f2
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2023-04-12 10:53:16 +0200

    add arguments to run gstreamers

--- scripts/rtcp_recv.sh
+++ scripts/rtcp_recv.sh
@@ -1,6 +1,7 @@
+# $0
 gst-launch-1.0 -v \
 rtpbin name=rtpbin \
-udpsrc address=127.0.0.1 port=5000 do-timestamp=true  caps="application/x-rtp,media=(string)video,clock-rate=(int)90000,encoding-name=(string)H264" ! rtpbin.recv_rtp_sink_0 \
-udpsrc address=127.0.0.1 port=5001 caps="application/x-rtcp" ! rtpbin.recv_rtcp_sink_0 \
+udpsrc address=$1 port=5000 do-timestamp=true  caps="application/x-rtp,media=(string)video,clock-rate=(int)90000,encoding-name=(string)H264" ! rtpbin.recv_rtp_sink_0 \
+udpsrc address=$1 port=5001 caps="application/x-rtcp" ! rtpbin.recv_rtcp_sink_0 \
 rtpbin. ! rtph264depay name=depay ! avdec_h264 ! videoconvert ! autovideosink \
-rtpbin.send_rtcp_src_0 ! udpsink port=5002 sync=false async=false host=127.0.0.1
+rtpbin.send_rtcp_src_0 ! udpsink port=5002 host=$1 sync=false async=false
