e69948c1cd631d0ab3a4c026e8dcd819611a56c5
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2023-09-22 16:31:56 +0200

    new screenshot

--- dashboard.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import sys, os
-import subprocess
-
-while True:
-    ret = subprocess.run(["python3", "main.py"] + sys.argv[1:])
-    if ret.returncode == 0:
-        sys.exit(0)
