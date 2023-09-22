b0512cd60bd2a3d9de9146aa2429eae9f01dcd45
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2023-09-22 13:45:01 +0200

    add ros scripts

---
+++ run_full_gui.sh
@@ -0,0 +1,4 @@
+
+sh ./run_ros1.sh python3 dashboard_robot.py configs/teleop_tiago.yaml layouts/layout_teleop.yaml &
+sh ./run_ros2.sh python3 dashboard_video.py configs/teleop_tiago.yaml layouts/layout_teleop.yaml 
+
